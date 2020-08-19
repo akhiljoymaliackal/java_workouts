aa237c4a0117e18c5a10522b10234f4ba6e41ad1
Akhil Joy Maliackal <akhilmaliackal@gmail.com>
2020-08-19 19:38:07 +0530

    Create Largest_of_three_numbers_in _two_lines.java

---
+++ Largest_of_three_numbers_in _two_lines.java
@@ -0,0 +1,12 @@
+package larning;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		int a = 100,b=10,c=30;
+		int op = ((a>b)?a:b)>c?((a>b)?a:b):c;
+		System.out.println("Largest of three number is : "+ op);
+
+	}
+
+}
